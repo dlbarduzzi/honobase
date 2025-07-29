dfd4123b26ccd0ae2f4333d14ac0adf0fbb2667d
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-07-29 16:58:16 -0500

    Add more tests

--- src/tools/strings/index.ts
+++ src/tools/strings/index.ts
@@ -1,5 +1,28 @@
-function capitalize(str: string) {
+export function lowercase(str: string) {
+  return str.toLowerCase()
+}
+
+export function uppercase(str: string) {
+  return str.toUpperCase()
+}
+
+export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-export { capitalize }
+export function strings(str: string) {
+  return {
+    hasNumber: () => {
+      return /[0-9]/.test(str)
+    },
+    hasSpecialChar: () => {
+      return /[!?@#$&^*_\-=+]/.test(str)
+    },
+    hasLowercaseChar: () => {
+      return /[a-z]/.test(str)
+    },
+    hasUppercaseChar: () => {
+      return /[A-Z]/.test(str)
+    },
+  }
+}
